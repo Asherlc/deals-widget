4ee516a8bb54121c2a2e2e6a6e1b2ecef11d328b
Asher Cohen <asherlc@asherlc.com>
2014-11-05 11:01:15 -0800

    img

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,28 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    copy: {
+      html: {
+        src: ['src/html/index.html'],
+        dest: 'dist/<%= pkg.name %>.wdgt/index.html'
+      },
+      img: {
+        files: [
+          {
+            src: ['src/img/Default.png'],
+            dest: 'dist/<%= pkg.name %>.wdgt/Default.png',
+          },
+          {
+            src: ['src/img/Icon.png'],
+            dest: 'dist/<%= pkg.name %>.wdgt/Icon.png',
+          }
+        ]
+      },
+      plist: {
+        src: ['Info.plist'],
+        dest: 'dist/<%= pkg.name %>.wdgt/Info.plist'
+      }
+    },
     concat: {
       options: {
         seperator: ';'
@@ -31,9 +53,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Default task(s).
-  grunt.registerTask('default', ['concat', 'uglify', 'clean']);
+  grunt.registerTask('default', ['copy', 'concat', 'uglify', 'clean']);
   
 
 }
